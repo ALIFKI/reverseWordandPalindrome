bd5f734bd11c44c801ac6f91699b1125e8bb7fd4
ALIFKI <mockingbirds233@gmail.com>
2020-06-05 14:12:15 +0700

    comment

--- reverseWord.js
+++ reverseWord.js
@@ -1,13 +1,16 @@
 function reverseWord(txt){
+    //validasi inputan supaya tidak kosong
     if (txt.length <= 1) {
+        //variable jika terjadi inputa error
         let message = {
             kode : '500',
             msg  : 'Inputan tidak valid'
         }
-
         return message
     } else {
+        //Memisahkan string jadi array,dibalik kemudian digabungkan kembali
         var teks = txt.split(' ').reverse().join(' ');
+        //Pesan jika berhasil reverse
         let data = {
             msg : teks
         }
